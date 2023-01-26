ebebf77829abacac8e9c9c29d03858b33328472f
Alex Thomas MacBook <alexthomas@gmail.local>
2023-01-26 17:05:25 +0000

    heroku

--- cart/models.py
+++ cart/models.py
@@ -5,10 +5,11 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     product = models.ManyToManyField(
         "products.Product",
-        related_name="cart",  
+        related_name="cart",
+        blank=True
     )
     owner = models.ForeignKey(
         "jwt_auth.User",
         related_name="cart",
         on_delete=models.CASCADE
-    )
+    ) 
