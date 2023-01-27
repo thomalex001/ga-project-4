3270dd19d72e1b66fdb3e7c57e8797e14c32c0ac
Alex Thomas MacBook <alexthomas@gmail.local>
2023-01-27 11:16:45 +0000

    added-css

--- project/settings.py
+++ project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'products',
     'type',
     'cart',
-    'corsheaders',
+    # 'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,8 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
+    # 'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
